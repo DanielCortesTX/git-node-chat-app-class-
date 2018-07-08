cfa4437d4f9795c88e6174f0ca7a32532ccf40c2
Daniel Cortes <d5t5c5@yahoo.com>
2018-07-08 17:36:32 -0500

    fixed launch problem?

--- server/server.js
+++ server/server.js
@@ -1,19 +1,15 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirname, '../public')
-
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express(); 
 
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-	res.render('index.html');
-});
-
 //express static middleware
 //port.listn 3000 message
 //start in terminal
-app.listen(3000, () => {
-	console.log('Started on port 3000');
+app.listen(port, () => {
+	console.log(`Server is up on ${port}`);
 })
